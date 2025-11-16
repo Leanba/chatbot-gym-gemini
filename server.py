645385c3b90a5f17a75c310464ce4bc63ee56e67
leanba <186583772+Leanba@users.noreply.github.com>
2025-11-16 16:31:26 -0300

    Fix sync webhook para Flask

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 import os
-import asyncio
 from flask import Flask, request
+import asyncio
 from bot import bot_app
 
 app = Flask(__name__)
 
-# Telegram usará este endpoint
-@app.post("/")
-async def webhook():
+@app.route("/", methods=["POST"])
+def webhook():
     update_data = request.get_json(force=True)
+    
     update = bot_app.bot._factory.update(update_data, bot_app.bot)
-    await bot_app.process_update(update)
+
+    # Ejecutamos el update en el event loop del bot
+    asyncio.get_event_loop().create_task(bot_app.process_update(update))
+
     return "OK", 200
 
 
