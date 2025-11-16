4e914793d548614476cb3f41c292efd59c6fa2f8
leanba <186583772+Leanba@users.noreply.github.com>
2025-11-16 16:26:29 -0300

    Fix webhook async server

--- server.py
+++ server.py
@@ -5,22 +5,15 @@ from bot import bot_app
 
 app = Flask(__name__)
 
-# Usamos el loop del bot
-loop = asyncio.get_event_loop()
-
-@app.route("/", methods=["POST"])
-def webhook():
-    update = request.get_json()
-
-    # Procesamos update con el bot
-    loop.create_task(bot_app.process_update(update))
-
+# Telegram usará este endpoint
+@app.post("/")
+async def webhook():
+    update_data = request.get_json(force=True)
+    update = bot_app.bot._factory.update(update_data, bot_app.bot)
+    await bot_app.process_update(update)
     return "OK", 200
 
-@app.route("/", methods=["GET"])
-def home():
-    return "Bot funcionando", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.getenv("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
